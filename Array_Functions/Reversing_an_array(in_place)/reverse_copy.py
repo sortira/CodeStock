1321ae5f7447d8d3dbfde8d2fc8e2185658ad14d
Mayank9207 <mayank95237@gmail.com>
2024-12-12 17:26:09 +0530

    Update and rename reverse_copy.py to reverse_in_place.py

--- Array_Functions/Reversing_an_array(in_place)/reverse_copy.py
+++
@@ -1,9 +0,0 @@
-def reverse_copy(vc):
-    return list(reversed(vc))  
-
-if __name__ == "__main__":
-    vc = [1, 2, 3]
-    rev = reverse_copy(vc)
-    for i in rev:
-        print(i, end=" ")
-    print()
