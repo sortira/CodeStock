6db849212772e0fb78f79f7c30ced138dad12781
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2024-12-12 23:17:08 +0530

    Update count_occurence.py

--- Array_Functions/Count_Occurence/count_occurence.py
+++ Array_Functions/Count_Occurence/count_occurence.py
@@ -1,15 +1,12 @@
-def Count_Occurences(arr, item):
+def Count_Occurences(arr):
     mpp = {}
     for i in arr:
         mpp[i] = mpp.get(i,0) + 1
 
-    count = mpp[item]
-    return count
+    return mpp
 
 if __name__ == "__main__":
     arr = [1,2,3,3,5,4,6,4,1,2]
-    val = 4
-    count = Count_Occurences(arr, val)
-    
-    print("Element:", val)
-    print("Occurence:", count)
+    mpp = Count_Occurences(arr)
+    for key, value in mpp.items():
+        print(key, value)
