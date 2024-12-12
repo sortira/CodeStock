90cc253e1a40ad358cc14c6a002e7b8626ab41de
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2024-12-12 23:12:21 +0530

    Update count_occurence.cpp

--- Array_Functions/Count_Occurence/count_occurence.cpp
+++ Array_Functions/Count_Occurence/count_occurence.cpp
@@ -5,22 +5,22 @@
 using namespace std;
 
 template <typename T>
-int Count_Occurences(vector<T> arr, T item)
+unordered_map<T, int> Count_Occurences(vector<T> arr)
 {
     unordered_map<T,int> mpp;
     for (T i: arr)
         mpp[i]++;
     
-    int count = mpp[item];
-    return count;
+    return mpp;
 }
 
 int main()
 {
     vector<int> vc = {1,2,3,5,4,4,6,3,2,3,7};
-    int val = 4;
-    int count = Count_Occurences(vc, val);
-    cout << "Element: " << val << endl;
-    cout << "Occurence: " << count << endl;
+    unordered_map<int, int> mpp = Count_Occurences(vc);
+    for(auto it: mpp)
+    {
+        cout << it.first << " " << it.second << endl;
+    }
     return 0;
 }
