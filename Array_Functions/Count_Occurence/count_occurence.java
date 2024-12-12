d1b18380a32ebb284e44d4de98c858e2f6e0d5b9
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2024-12-12 23:15:54 +0530

    Update count_occurence.java

--- Array_Functions/Count_Occurence/count_occurence.java
+++ Array_Functions/Count_Occurence/count_occurence.java
@@ -3,13 +3,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-    public static <T> int countOccurrences(List<T> arr, T item) {
+    public static <T> Map<T, Integer> Count_Occurrences(List<T> arr) {
         HashMap<T, Integer> mpp = new HashMap<>();
         for (T i : arr) {
             mpp.put(i, mpp.getOrDefault(i, 0) + 1);
         }
         
-        return mpp.getOrDefault(item, 0);
+        return mpp;
     }
 
     public static void main(String[] args) {
@@ -26,9 +26,10 @@ public class Main {
         vc.add(3);
         vc.add(7);
         
-        int val = 4;
-        int count = countOccurrences(vc, val);
-        System.out.println("Element: " + val);
-        System.out.println("Occurrence: " + count);
+        Map<Integer, Integer> mpp = Count_Occurrences(vc);
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) 
+        {
+            System.out.println(it.getKey() + " " + it.getValue());
+        }
     }
 }
