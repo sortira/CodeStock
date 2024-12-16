551f648ca66a2baf952702701b4276157b9a7231
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2024-12-16 11:06:53 +0530

    Update gcd_euclidean_algorithm.py

--- Number_Theory_Functions/GCD_Using_Euclidean_Algorithm/gcd_euclidean_algorithm.py
+++ Number_Theory_Functions/GCD_Using_Euclidean_Algorithm/gcd_euclidean_algorithm.py
@@ -1,11 +1,14 @@
 def gcd(a, b):
     if a == 0 and b == 0:
-        return -1  # Undefined for (0, 0)
+        return 0
     if a == 0:
         return b
     if b == 0:
         return a
 
+    if (a<0) a = -a
+    if (b<0) b = -b
+    
     while b != 0:
         a, b = b, a % b
     return a
@@ -15,7 +18,4 @@ if __name__ == "__main__":
     b = 37
 
     ans = gcd(a, b)
-    if ans == -1:
-        print("GCD is undefined for (0, 0)")
-    else:
-        print(ans)
+    print(ans)
