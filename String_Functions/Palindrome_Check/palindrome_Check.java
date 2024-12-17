fb6c8d8ae5c1086acbed21de26b326f0efbe089e
KakarotDB <mrigajpersonal@gmail.com>
2024-12-17 21:45:36 +0530

    Changed palindrome_Check files to use two pointer method instead of reversing the string.

--- String_Functions/Palindrome_Check/palindrome_Check.java
+++ String_Functions/Palindrome_Check/palindrome_Check.java
@@ -1,31 +1,26 @@
 
 
 public class palindrome_Check {
-    public String reverse_String(String string)
+    public boolean check_for_palindrome(String string)
     {
-        int length = string.length(); // length of the string
-        String reversed = "";
-        for (int i = length-1; i >=0; i--) {
-            reversed+=string.charAt(i); //we reverse the string by adding characters from the end of the string.
-
+        int length = string.length();
+        int left_pointer = 0;
+        int right_pointer = length -1;
+        while (left_pointer<right_pointer)
+        {
+            if (string.charAt(left_pointer)!=string.charAt(right_pointer))
+                return false;
+            left_pointer++;
+            right_pointer--;
         }
-        return reversed;
-    }
-    public boolean check_for_palindrome(String string)//checking for palindrome
-    {
-        palindrome_Check obj = new palindrome_Check();//creating object
-        String reversed = obj.reverse_String(string);//reversing the string by calling previously defined method "reverse_String"
-        if (string.compareToIgnoreCase(reversed)==0)//.compareToIgnoreCase() function returns 0 if both strings are identical, ignoring uppercase or lowercase.
-            return true;
-        else
-            return false;
-
+        return true;
     }
     public static void main(String[] args) {
         palindrome_Check obj = new palindrome_Check();
         String example_palindrome = "racecar";
         System.out.println(obj.check_for_palindrome(example_palindrome));
         System.out.println(obj.check_for_palindrome("NotAPalindrome"));
+
 
     }
 }
