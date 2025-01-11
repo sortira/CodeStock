64d11014493ec6b487945c859c410f94b98c25ef
Mayank9207 <mayank95237@gmail.com>
2025-01-11 11:04:34 +0530

    Update Z_algorithm(pattern_check).java

--- String_Functions/Z_algorithm(pattern_check)/Z_algorithm(pattern_check).java
+++ String_Functions/Z_algorithm(pattern_check)/Z_algorithm(pattern_check).java
@@ -43,8 +43,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String givenstring = "abcde";
-        String pattern = "";
+        String givenstring = "world peace";
+        String pattern = "world";
 
         // Perform pattern search using Z algorithm
         List<Integer> result = zfunction(givenstring, pattern);
